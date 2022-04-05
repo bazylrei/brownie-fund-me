ebef6a3943c170359f2bfe6f99bd326cc0f1b95e
Bazyl Reinstein <bazylrei@gmail.com>
2022-04-06 00:31:50 +0100

    initial commit

---
+++ scripts/deploy.py
@@ -0,0 +1,33 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    deploy_mocks,
+    get_account,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+from web3 import Web3
+
+
+def deploy_fund_me():
+    account = get_account()
+
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    should_verify = config["networks"][network.show_active()]["verify"]
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=should_verify,
+    )
+    print(f"Contract deployed to {fund_me.address}")
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
